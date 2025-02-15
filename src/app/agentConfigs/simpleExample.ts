aa5b6e71e6c23fe52698532bdeebda11a1e68af7
Kevin Hass <kevinhass@Kevins-MacBook-Pro-152.local>
2025-02-15 15:16:29 -0500

    Created struggling student and teacher personas

--- src/app/agentConfigs/simpleExample.ts
+++ src/app/agentConfigs/simpleExample.ts
@@ -2,33 +2,49 @@ import { AgentConfig } from "@/app/types";
 import { injectTransferTools } from "./utils";
 
 // Define agents
-const haiku: AgentConfig = {
-  name: "haiku",
-  publicDescription: "Agent that writes haikus.", // Context for the agent_transfer tool
+// const haiku: AgentConfig = {
+//   name: "haiku",
+//   publicDescription: "Agent that writes haikus.", // Context for the agent_transfer tool
+//   instructions:
+//     "Ask the user for a topic, then reply with a haiku about that topic.",
+//   tools: [],
+// };
+
+// const greeter: AgentConfig = {
+//   name: "greeter",
+//   publicDescription: "Agent that greets the user.",
+//   instructions:
+//     "Please greet the user and ask them if they'd like a Haiku. If yes, transfer them to the 'haiku' agent.",
+//   tools: [],
+//   downstreamAgents: [haiku],
+// };
+
+let teacher: AgentConfig;
+let timmy: AgentConfig;
+
+teacher = {
+  name: "teacher",
+  publicDescription: "7th grade math teacher",
   instructions:
-    "Ask the user for a topic, then reply with a haiku about that topic.",
+    "You are Ms. Davis, the smart 7th grade math teacher. Please read the previous conversation context. Please only tell the students that they have made a mistake. Do not solve any part of the question yourself. IMMEDIATELY transfer the user to the 'timmy' agent",
   tools: [],
+  downstreamAgents: [],
 };
 
-const greeter: AgentConfig = {
-  name: "greeter",
-  publicDescription: "Agent that greets the user.",
+timmy = {
+  name: "timmy",
+  publicDescription: "7th grader requesting help from user",
   instructions:
-    "Please greet the user and ask them if they'd like a Haiku. If yes, transfer them to the 'haiku' agent.",
+    "If the user makes any math mistake, IMMEDIATELY transfer the user to the 'teacher' agent. You are timmy, the dumbest 7th grader in your class. Please ask help from the user to solve the question 2x + 3 = 7 on your math homework. Do not figure out any part of the question yourself or correct anything.",
   tools: [],
-  downstreamAgents: [haiku],
+  downstreamAgents: [],
 };
 
-const tutor: AgentConfig = {
-  name: "tutor",
-  publicDescription: "Agent that is a math tutor.",
-  instructions:
-    "You are a math tutor for a 7th grader. Please help the 7th grader to work through the problem: 2x + 3 = 7",
-  tools: [],
-  downstreamAgents: [haiku],
-};
+
+timmy.downstreamAgents = [teacher];
+teacher.downstreamAgents = [timmy];
 
 // add the transfer tool to point to downstreamAgents
-const agents = injectTransferTools([greeter, haiku, tutor]);
+const agents = injectTransferTools([timmy, teacher]);
 
 export default agents;
